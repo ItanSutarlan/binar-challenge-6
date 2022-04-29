27a6e28c0519f90ce962e40a340c14208534d991
Itan Sutarlan <sutarlanitan@gmail.com>
2022-04-29 08:44:08 +0700

    Added OpenAPI and modified app.js

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require('express');
+const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+
+const apiRouter = require('./server/routes');
+const errorHandler = require('./server/middlewares/errorHandler');
+const swaggerDocument = require('./restful_api.json');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// docs
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
